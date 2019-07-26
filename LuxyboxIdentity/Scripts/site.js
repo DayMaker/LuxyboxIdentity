e133ef5769b013d0061b9d82d65f7e82bd80013f
gamze gezer <gamze_gezer34@hotmail.com>
2019-07-26 10:21:37 +0300

    input button ekleme

---
+++ LuxyboxIdentity/Scripts/site.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("#btnDecrease").on("click", function () {
+        var value = parseInt($("#inputPrice").val());
+
+        value = value - 1;
+
+        if (value < 1)
+            value = 1;
+
+        $("#inputPrice").val(value)
+    });
+
+    $("#btnIncrease").on("click", function () {
+        var value = parseInt($("#inputPrice").val());
+
+        value = value + 1;
+
+        if (value > 10)
+            value = 10;
+
+        $("#inputPrice").val(value)
+    });
+
+});

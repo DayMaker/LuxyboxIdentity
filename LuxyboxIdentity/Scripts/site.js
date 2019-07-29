83993c7a046d8088ac877aef68cde3ecf490749b
emre başer <ebaser3816@gmail.com>
2019-07-29 14:57:15 +0300

    js değişiklikleri

--- LuxyboxIdentity/Scripts/site.js
+++ LuxyboxIdentity/Scripts/site.js
@@ -1,71 +1,75 @@
 ﻿$(document).ready(function () {
-    $("#btnDecrease").on("click", function () {
-        var value = parseInt($("#inputPrice").val());
+    $(".btn-decrease").on("click", function () {
+        var $productItem = $(this).closest(".product-item");
+        var $inputQuantity = $productItem.find(".product-quantity");
+        var productId = $productItem.attr("data-product-id");
+        var productPrice = $productItem.attr("data-product-price");
+
+        var value = parseInt($inputQuantity.val());
         value = value - 1;
         if (value < 1)
             value = 1;
 
-        $("#inputPrice").val(value);
-
-        var productId = $(this).closest(".product-item").attr("data-product-id");
+        $inputQuantity.val(value);
         updateQuantity(productId, value);
-        
+
         productPrice = productPrice.replace(",", ".");
         productPrice = (parseFloat(productPrice) * value).toFixed(2);
-        $(this).closest(".product-item").find(".price").text(productPrice);
-});
-       
+        $productItem.find(".product-price").text(productPrice);
     });
 
-    $("#btnIncrease").on("click", function () {
-        var value = parseInt($("#inputPrice").val());
- 
-        value = value + 1;
+    $(".btn-increase").on("click", function () {
+        var $productItem = $(this).closest(".product-item");
+        var $inputQuantity = $productItem.find(".product-quantity");
+        var productId = $productItem.attr("data-product-id");
+        var productPrice = $productItem.attr("data-product-price");
 
+        var value = parseInt($inputQuantity.val());
+        value = value + 1;
         if (value > 10)
             value = 10;
 
-        $("#inputPrice").val(value);
-       
-        var productId = $(this).closest(".product-item").attr("data-product-id");
+        $inputQuantity.val(value);
         updateQuantity(productId, value);
 
-
-        var productPrice = $(this).closest(".product-item").attr("data-product-price");
         productPrice = productPrice.replace(",", ".");
         productPrice = (parseFloat(productPrice) * value).toFixed(2);
-        $(this).closest(".product-item").find(".price").text(productPrice);
-   
-        
+        $productItem.find(".product-price").text(productPrice);
     });
-    $("#btnDelete").on("click", function () {
-      var productId = $(this).closest(".product-item").attr("data-product-id");
-        
-       
-        updateDelete(productId);
 
+
+    $(".btn-delete").on("click", function () {
+        var $productItem = $(this).closest(".product-item");
+        var productId = $productItem.attr("data-product-id");
+        updateDelete(productId, $productItem);
     });
-    
+});
 
-    function updateQuantity(productId, quantity) {
-        $.ajax({
-            method: "POST",
-            url: "/Home/CartItemQuantityUpdate",
-            data: { productId: productId, quantity: quantity}
-        })
-            .done(function (msg) {
-            });
-    }
-    function updateDelete(productId) {
-        $.ajax({
-            url: "/Home/CartItemDeleteUpdate",
-            type: 'POST',
-            data: { productId: productId },
-            success: function (result) {
-                // Do something with the result
-            }
+
+
+
+function updateQuantity(productId, quantity) {
+    $.ajax({
+        method: "POST",
+        url: "/Home/CartItemQuantityUpdate",
+        data: { productId: productId, quantity: quantity }
+    })
+        .done(function (msg) {
         });
-    }
+}
+function updateDelete(productId, $productItem) {
+    $.ajax({
+        url: "/Home/CartItemDeleteUpdate",
+        type: 'POST',
+        data: { productId: productId },
+        success: function (r) {
+            if (r.result === true) {
+                $productItem.remove();
+            }
+            // Do something with the result
+        }
+    });
+}
 
 
 
